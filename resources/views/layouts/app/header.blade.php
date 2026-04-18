7011e2a02c39934078a4d94327c3e1c9166348b1
Ananda Zaka <anandazaka23@gmail.com>
2026-04-18 22:39:46 +0700

    Remove Filament admin resources and assets
    Menghapus implementasi admin berbasis Filament beserta semua aset terkait. Seluruh class Resource Filament berikut Pages, Schemas, dan Tables untuk Mentors, Programs, Promos, Registrations, dan Schedules telah dihapus. Selain itu, AdminPanelProvider Filament serta provider lain seperti AppServiceProvider dan FortifyServiceProvider juga ikut dihapus.
    
    Registrasi panel Filament di bootstrap/providers.php telah dicabut, dan package Filament dihapus dari composer.json. Aset publik Filament (JS/CSS) yang sebelumnya di-generate juga sudah dibersihkan. File resources/css/app.css dan resources/views/layouts/app/header.blade.php turut diperbarui untuk menyesuaikan dengan penghapusan ini.
    
    Perubahan ini secara keseluruhan menghilangkan seluruh fitur admin Filament beserta scaffolding-nya, jadi pastikan langkah instalasi dan deployment juga diperbarui sesuai kondisi terbaru proyek.

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -19,7 +19,7 @@
 
             <flux:navbar class="me-1.5 space-x-0.5 rtl:space-x-reverse py-0!">
                 <flux:tooltip :content="__('Search')" position="bottom">
-                    <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="magnifying-glass" href="#" :label="__('Search')" />
+                    <flux:navbar.item class="h-10! [&>div>svg]:size-5" icon="magnifying-glass" href="#" :label="__('Search')" />
                 </flux:tooltip>
                 <flux:tooltip :content="__('Repository')" position="bottom">
                     <flux:navbar.item
